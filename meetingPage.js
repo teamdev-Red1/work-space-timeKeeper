1825ed653466da2f98ad93eccfa3e768066b0fa5
gyantic <kkimura330mp@gmail.com>
2024-12-23 12:06:58 +0900

    create timer logic and page transition

--- meetingPage.js
+++ meetingPage.js
@@ -1,29 +1,53 @@
-//ダウンカウンターの実装
-let countdownTime = 10 * 60;  //例として10分
+document.addEventListener('DOMContentLoaded', function () {
+  const currentPage = window.location.pathname.split('/').pop();
+  if (currentPage === 'index.html') {
+    //index.htmlでの処理
+    const startButton = document.getElementById('start-button');
+    startButton.addEventListener('click', function () {
+      const agenda = document.getElementById('agenda-container').value;
+      const minutes = document.getElementById('timer-input').value;
 
-const countdownElement = document.getElementById('countdown')
+      //localStorageにデータを保存
+      localStorage.setItem('agenda', agenda);
+      localStorage.setItem('minutes', minutes);
 
-//時間は二桁表示    ex)5分："05"
-function formatTime(seconds) {
-  const minutes = Math.floor(seconds / 60);
-  const remainSeconds = seconds % 60;
-  return `${minutes.toString().padStart(2, '0')}:${remainSeconds.toString().padStart(2, '0')}`;
-}
-
-function updateCountdown(){
-  if (countdownTime > 0) {
-    countdownTime--;
-    countdownElement.textContent = formatTime(countdownTime);
+      //meetingPage.htmlに移動
+      window.location.href = "meetingPage.html";
+    });
   }
-  else {
-    clearInterval(countdownInterval);
-    countdownElement.textContent = "00:00";
+  else if (currentPage === 'meetingPage.html') {
+    //meetingPage.htmlの処理
+    const agendaDisplay = document.getElementById('agendaDisplay');
+    const timerDisplay = document.getElementById('timer');
+
+    //localStorageからデータ取得
+    const agenda = localStorage.getItem('agenda');
+    const minutes = localStorage.getItem('minutes');
+
+    //アジェンダの表示
+    agendaDisplay.textContent = agenda;
+
+    //ダウンカウンターの実装
+    let remainTime = parseInt(minutes, 10)* 60;
+
+    const updateTimer = () => {
+      const mins = Math.floor(remainTime / 60);
+      const secs = remainTime % 60;
+      timerDisplay.textContent = `${String(mins).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+      //ここに残り時間ごとのメッセージをつけ足していく
+      if (remainTime > 0) {
+        remainTime -= 1;
+        setTimeout(updateTimer, 1000);
+      } else {
+        alert('終了しました!!');
+      }
+    };
+    updateTimer();
+
+    const backButton = document.getElementById('backButton');
+    backButton.addEventListener('click', function () {
+      //meetingPage.htmlに移動
+      window.location.href = "index.html";
+    });
   }
-}
-
-
-// 1秒ごとにカウントダウンを更新
-const countdownInterval = setInterval(updateCountdown, 1000);
-
-// 初期値を表示
-countdownElement.textContent = formatTime(countdownTime);
+});
