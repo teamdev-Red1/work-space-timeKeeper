74ce36b3dbb70de6473222b40d0d298a29dc386d
tantan226 <talumichi@gmail.com>
2024-12-26 19:06:53 +0900

    Remove seconds ele. Add slider scale.

--- main.js
+++ main.js
@@ -2,15 +2,29 @@ const  CONFIG = {
     minutesInput:document.getElementById("minutes-input"),
     secondsInput:document.getElementById("seconds-input"),
     minutesData:document.getElementById("minutes-data"),
-    secondsData:document.getElementById("seconds-data")
+    secondsData:document.getElementById("seconds-data"),
+    sliderMarker:document.getElementById("markers")
 }
 
 class View{
+    //スライダーとタイマーの連動
     static slider(time,input){
         input.addEventListener("input",function(){
-            time.textContent = input.value;
+            time.value = input.value;
         })
+    }
+    //スライダーの目盛りを作る
+    static createMarkers(maxValue){
+        const ele = CONFIG.sliderMarker;
+        
+        
+        for(let i = 5;i < maxValue; i += 5){
+            const marker = document.createElement("option");
+            marker.value = i;
+
+            ele.appendChild(marker);
+        }
     }
 }
 View.slider(CONFIG.minutesData,CONFIG.minutesInput);
-View.slider(CONFIG.secondsData,CONFIG.secondsInput);
+View.createMarkers(120);
