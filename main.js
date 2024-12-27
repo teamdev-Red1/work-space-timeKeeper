2ea88d91f64d9af634785b1300c1c53007dbc6af
tantan226 <talumichi@gmail.com>
2024-12-27 20:31:00 +0900

    Create add-btn and remove-btn. remove slider marker.

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 const  CONFIG = {
     minutesInput:document.getElementById("minutes-input"),
-    secondsInput:document.getElementById("seconds-input"),
     minutesData:document.getElementById("minutes-data"),
-    secondsData:document.getElementById("seconds-data"),
-    sliderMarker:document.getElementById("markers")
+
 }
 
 class View{
@@ -13,18 +11,54 @@ class View{
             time.value = input.value;
         })
     }
-    //スライダーの目盛りを作る
-    static createMarkers(maxValue){
-        const ele = CONFIG.sliderMarker;
-        
-        
-        for(let i = 5;i < maxValue; i += 5){
-            const marker = document.createElement("option");
-            marker.value = i;
+    //アジェンダを追加する
+    static createAgenda(n){
+        //n個までアジェンダが作れる
+        if(!Controller.isMaxOrLessAgenda(n)){
+            alert(`アジェンダの最大は${n}個までです`);
+        }else{
+        const input = document.createElement("input");
+        const target = document.getElementById("agenda-container");
 
-            ele.appendChild(marker);
+        input.classList.add("agenda-input","mb-3");
+        input.placeholder = "アジェンダを入力してください";
+        target.append(input);
         }
+    }
+    //アジェンダを削除するメソッド
+    static removeAgenda(){
+        const agenda = document.querySelectorAll(".agenda-input");
+        const target = agenda.item(agenda.length - 1);
+        if(agenda.length > 1){
+            target.remove();
+        }
+        
+    }
+}
+class Controller{
+    //add btn event
+    static addBtn(){
+       const btn = document.getElementById("add-btn");
+
+        btn.addEventListener("click",function(){
+            View.createAgenda(3);
+        })
+    }
+    static removeBtn(){
+        //remove btn event
+        const btn = document.getElementById("remove-btn");
+
+        btn.addEventListener("click",function(){
+            View.removeAgenda();
+        })
+    }
+    //最大値nよりアジェンダの数が少ないか
+    static isMaxOrLessAgenda(n){
+        const numberOfAgenda = document.querySelectorAll(".agenda-input").length;
+        console.log(numberOfAgenda);
+        return numberOfAgenda < n;
     }
 }
 View.slider(CONFIG.minutesData,CONFIG.minutesInput);
-View.createMarkers(120);
+Controller.addBtn();
+Controller.removeBtn();
